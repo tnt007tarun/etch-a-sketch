5c7cdaff80af15ec46e9c7da45595be1657554f7
Tarun <tnt.tarun@gmail.com>
2015-05-20 00:04:32 -0400

    Update jquery.js
    The reset only works perfectly if we ask for 100 squares/side.. still working this out

--- jquery.js
+++ jquery.js
@@ -1,1 +1,48 @@
+$(document).ready (function () {
 
+	var side = 16;
+
+	for (var i=0; i<side; i++) {
+		$('#tab').append('<tr><td></td></tr>');
+	}
+
+	for (var j=0; j<side-1; j++) {
+		$('tr').append($('<td></td>'));
+	}
+
+	$("td").on('mouseenter', function() {
+		$(this).css({"background-color": "black"});
+	});
+
+	$("td").on('mouseleave', function() {
+		$(this).css({"background-color": "#1A1A19"});
+	});
+
+$('button').on('click', function() {
+	var newside=prompt("How many squares per side?");
+	$('tr').remove();
+	$(side).val('newside');
+	var num = 800/newside;
+
+	for (var k=0; k<newside; k++) {
+		$('#tab').append('<tr><td></td></tr>');
+	}
+
+	for (var l=0; l<newside-1; l++) {
+		$('tr').append($('<td></td>'));
+	}
+
+	$('tr').css("height", "8");
+	$('td').css({"width" : "8", "background-color" : "#FFFFEB"});
+
+		$("td").on('mouseenter', function() {
+		$(this).css({"background-color": "black"});
+	});
+
+	$("td").on('mouseleave', function() {
+		$(this).css({"background-color": "#1A1A19"});
+	});
+});
+
+});
+	
